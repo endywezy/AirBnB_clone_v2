0f772ab4fae5e7afc5652863f55418186c3a1d52
endywezy <endywezy@gmail.com>
2024-03-10 17:39:39 -0700

    AirBnB Clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,6 @@ class FileStorage:
     FileStorage class for storing, serializing and deserializing data
     """
     __file_path = "file.json"
-
     __objects = {}
 
     def new(self, obj):
@@ -27,9 +26,7 @@ class FileStorage:
          <obj class name>.id.
         """
         obj_cls_name = obj.__class__.__name__
-
         key = "{}.{}".format(obj_cls_name, obj.id)
-
         FileStorage.__objects[key] = obj
 
 
@@ -47,12 +44,9 @@ class FileStorage:
         JSON format and saves it to the file specified by __file_path.
         """
         all_objs = FileStorage.__objects
-
         obj_dict = {}
-
         for obj in all_objs.keys():
             obj_dict[obj] = all_objs[obj].to_dict()
-
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(obj_dict, file)
 
@@ -64,14 +58,10 @@ class FileStorage:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 try:
                     obj_dict = json.load(file)
-
                     for key, value in obj_dict.items():
                         class_name, obj_id = key.split('.')
-
                         cls = eval(class_name)
-
                         instance = cls(**value)
-
                         FileStorage.__objects[key] = instance
                 except Exception:
                     pass
