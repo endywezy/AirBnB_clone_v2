f83132baece110fd7d97ff2b839f53647e3a584d
endywezy <endywezy@gmail.com>
2024-05-06 14:04:33 -0700

    Configuration management

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['18.209.20.255', '34.73.76.135']
+env.hosts = ['100.26.57.162', '100.26.120.217']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
