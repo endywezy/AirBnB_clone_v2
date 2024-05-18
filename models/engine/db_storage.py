86f57b3b8495428af1c5efa9223f53f8ad520f21
endywezy <endywezy@gmail.com>
2024-05-18 22:27:24 +0000

    Configuration management

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
 
 class DBStorage:
     """This class manages database storage of hbnb models"""
@@ -10,14 +20,6 @@ class DBStorage:
     def __init__(self):
         """Instantiates a new model"""
         import os
-        from sqlalchemy import (create_engine)
-        from models.base_model import Base, BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.amenity import Amenity
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
             os.getenv('HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'),
             os.getenv('HBNB_MYSQL_HOST'), os.getenv('HBNB_MYSQL_DB')),
@@ -28,73 +30,36 @@ class DBStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
         new_dict = {}
         if cls is None:
-            for item in self.__session.query(User).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(State).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(City).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Amenity).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Place).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Review).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
+            for cls in [User, State, City, Amenity, Place, Review]:
+                objs = self.__session.query(cls).all()
+                for obj in objs:
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    new_dict[key] = obj
         else:
-            for item in self.__session.query(cls).all():
-                key = "{}.{}".format(item.__class__.__name__,  item.id)
-                new_dict[key] = item
-        self.__session.close()
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                new_dict[key] = obj
         return new_dict
 
     def new(self, obj):
         """Adds new object to storage"""
-        from sqlalchemy.orm import sessionmaker
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
         self.__session.add(obj)
 
     def save(self):
         """commit all changes"""
         self.__session.commit()
-        # self.__session.close()
 
     def delete(self, obj=None):
         """Delete object from database"""
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
         """create all tables in the database"""
-        from sqlalchemy.orm import scoped_session
-        from sqlalchemy.orm import sessionmaker
-        from models.base_model import BaseModel, Base
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
         Base.metadata.create_all(self.__engine)
-
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
@@ -103,3 +68,9 @@ class DBStorage:
     def close(self):
         """ Closes a session """
         self.__session.close_all()
+
+
+# Instance of the DBStorage class
+storage = DBStorage()
+storage.reload()
+
