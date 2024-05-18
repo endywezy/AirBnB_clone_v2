37d016374df2dea56f9a7548fa9a9fbc36a0306e
endywezy <endywezy@gmail.com>
2024-05-18 21:39:38 +0000

    Configuration management

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+import os
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+
+
+    def __init__(self):
+        """Initialize the FileStorage class and create necessary directories and files"""
+        self.setup_directories_and_files()
+
+
+    def setup_directories_and_files(self):
+        """Create templates directory and 5-number.html file if they don't exist"""
+        templates_dir = os.path.join(os.path.dirname(__file__), '..', 'web_flask', 'templates')
+        file_path = os.path.join(templates_dir, '5-number.html')
+
+        try:
+            os.makedirs(templates_dir, exist_ok=True)
+            with open(file_path, 'a'):
+                os.utime(file_path, None)
+        except Exception as e:
+            print(f"An error occurred while setting up directories and files: {e}")
+
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -15,9 +34,11 @@ class FileStorage:
                     if isinstance(v, cls)}
         return FileStorage.__objects
 
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -27,6 +48,7 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -52,6 +74,7 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
+
     def delete(self, obj=None):
         """Delete object from file storage"""
         if obj is not None:
@@ -59,6 +82,7 @@ class FileStorage:
                 if obj == v:
                     del self.__objects[k]
 
+
     def close(self):
         """ Reload storage dictionary from file"""
-        reload(self)
+        self.reload()
