b9bb671daf410f40e09d1a72ea64f9c3edd4732a
endywezy <endywezy@gmail.com>
2024-05-18 21:19:36 +0000

    Web Flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -65,7 +65,9 @@ def number_odd_or_even(n):
     Display an HTML page only if n is an integer
     """
     odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
+    return render_template(
+        '6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even
+    )
 
 
 if __name__ == "__main__":
