9b8bfadde3190977eb2f2a8237964c114dbe7f70
endywezy <endywezy@gmail.com>
2024-05-18 09:15:20 +0000

    Web Flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -15,12 +15,14 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Route to display 'HBNB'.
     """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -31,6 +33,7 @@ def c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
@@ -40,6 +43,7 @@ def python_text(text):
     """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
