3eb5c459d3a82c82e61f3fa41d93c82916d6d20a
endywezy <endywezy@gmail.com>
2024-05-18 09:10:26 +0000

    Web Flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route to display 'Hello HBNB!'.
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route to display 'HBNB'.
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route to display 'C ' followed by the value of the text variable.
+    Replace underscores with spaces.
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Route to display 'Python ' followed by the value of the text variable.
+    Replace underscores with spaces. The default value of text is 'is cool'.
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
