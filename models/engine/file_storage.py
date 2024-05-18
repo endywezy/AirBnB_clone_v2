615d6def8d74d18b5cc4ba5f2f46c14ee331b3b8
endywezy <endywezy@gmail.com>
2024-05-18 22:10:58 +0000

    Configuration management

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
-
-    def __init__(self):
-        """Initialize the FileStorage class and create necessary directories and files"""
-        self.setup_directories_and_files()
-
-    def setup_directories_and_files(self):
-        """Create templates directory and 5-number.html file if they don't exist"""
-        templates_dir = os.path.join(os.path.dirname(__file__), '..', 'web_flask', 'templates')
-        file_path = os.path.join(templates_dir, '5-number.html')
-
-        try:
-            os.makedirs(templates_dir, exist_ok=True)
-            with open(file_path, 'a'):
-                os.utime(file_path, None)
-        except Exception as e:
-            print(f"An error occurred while setting up directories and files: {e}")
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -34,8 +24,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update(
-            {obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -79,4 +68,4 @@ class FileStorage:
 
     def close(self):
         """Reload storage dictionary from file"""
-        self.reload()
+        reload(self)
