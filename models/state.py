d0ac117701cb4d6cbbd4e6cea1a6af6c14703394
endywezy <endywezy@gmail.com>
2024-05-20 16:33:40 -0700

    Configuration management

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
